cbe9511838570132a7f6c71b6fe5fdf6f85619f0
Joaquin Briceno <joaquin_briceno@yahoo.com>
2017-08-07 12:55:32 -0700

    added grunt config file

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = function (grunt) {
+  // Show elapsed time at the end
+  require('time-grunt')(grunt);
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      gruntfile: {
+        src: ['Gruntfile.js']
+      },
+      js: {
+        src: ['*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    mochacli: {
+      options: {
+        reporter: 'spec',
+        bail: true
+      },
+      all: ['test/*.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      js: {
+        files: '<%= jshint.js.src %>',
+        tasks: ['jshint:js', 'mochacli']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'mochacli']
+      }
+    },
+    sg_release: {
+      options: {
+        skipBowerInstall: true,
+        developBranch: 'develop',
+        masterBranch: 'master',
+        files: [
+          'package.json'
+        ],
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['-a'], // '-a' for all files
+        pushTo: 'origin'
+      }
+    }
+  });
+
+  grunt.registerTask('test', ['jshint', 'mochacli']);
+
+  grunt.registerTask('release', ['test', 'sg_release']);
+
+  grunt.registerTask('default', ['menu']);
+};
