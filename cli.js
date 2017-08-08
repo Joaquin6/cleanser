68ac78def43ecdf646b9c9f74be3a20b4230f83f
Joaquin Briceno <joaquin_briceno@yahoo.com>
2017-08-08 15:42:46 -0700

    fix(tests): fixed unit tests and path ex/in clusions

--- cli.js
+++ cli.js
@@ -1,19 +1,129 @@
 #!/usr/bin/env node
 
-'use strict';
+const colors = require('colors');
+const clear = require('clear');
+const CLI = require('clui');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+const Spinner = CLI.Spinner;
+const meow = require('meow');
+const _ = require('underscore');
+const prompter = require('./src/lib/prompter');
+const Cleanser = require('.');
+const pkgname = require('./package.json').name;
+const debug = require('debug')(pkgname);
 
-var meow = require('meow');
-var cleanDir = require('.');
+let helpmsg = `
+  Usage
+    $ cleandir            Removes all files from the current folder.
+    $ cleandir <path>     Removes all files from specified dir path.
 
-var cli = meow({
-  help: [
-    'Usage',
-    '  cleandir           Removes all files from the current folder.',
-    '  cleandir <path>    Removes all files from specified dir path.',
-    '',
-    'Example',
-    '  cleandir'
-  ].join('\n')
+  Options
+    -c, --config          Pass in a configuration file location
+    -i, --includes        Include source paths to delete
+    -G, --ignore          Ignore source paths, avoid deletion
+
+  Example
+    $ cleandir . --includes coverage, package-lock.json
+  `;
+let cleanser, cli;
+
+function getCLI() {
+  const cli = meow({
+    alias: {
+      c: 'config',
+      i: 'includes'
+    },
+    help: helpmsg
+  });
+
+  return cli;
+}
+
+function removeSources(callback) {
+  const status = new Spinner("Running Cleanser.....");
+  status.start();
+
+  cleanser.removeSourcePaths(err => {
+    status.stop();
+    if (err) return callback(err);
+    callback(null, {
+      toExit: true
+    });
+  });
+}
+
+function commenceIntroduction() {
+  clear();
+  console.log(
+    colors.yellow(
+      figlet.textSync('Cleanser', {
+        horizontalLayout: 'full'
+      })
+    )
+  );
+  if (process.env.DEBUG && process.env.DEBUG.includes(pkgname))
+    Error.stackTraceLimit = 50;
+}
+
+function promptQuestions(callback) {
+  /**
+   * Prompt questions to double check that the include paths are
+   * in fact suppose to be deleted. This is just an extra check for
+   * the end user to confirm.
+   */
+  const dblcheck = prompter.getInitialQuestions(cleanser);
+
+  /**
+   * Only prompt the question if there are choices to check from.
+   */
+  if (!_.isEmpty(dblcheck.choices)) {
+    inquirer.prompt(dblcheck)
+    .then(answers => {
+      if (!_.isEmpty(answers)) {
+        debug('Moving these to Ignore: %O', answers.dblChkSrcRemoval);
+        return cleanser.moveToIgnore(answers.dblChkSrcRemoval, (err, sourcePaths) => {
+          if (err) return callback(err);
+          return sourcePaths;
+        });
+      }
+      return cleanser.getSourcePaths();
+    })
+    .then(() => removeSources(callback))
+    .catch(callback);
+  } else {
+    removeSources(callback);
+  }
+}
+
+function initializeCleanser(callback) {
+  cli = getCLI();
+  debug('CLI Object: %O', cli);
+  if (cli.input[0] === 'help') {
+    return cli.showHelp(0);
+  }
+  cleanser = new Cleanser(cli.input[0], callback);
+}
+
+function answersCallback(err, res) {
+    if (err) {
+        debug('An error has Occurred: ', err);
+    } else {
+        let msg = 'All done!';
+        if (res.message) msg = res.message;
+        debug(colors.green(msg));
+    }
+    if (res && res.toExit)
+        process.exit(0);
+    else
+        promptQuestions(answersCallback);
+}
+
+commenceIntroduction();
+initializeCleanser(err => {
+  if (err) {
+    debug('%s: %O', colors.red('Cleanser CLI Error'), err);
+    process.exit(0);
+  }
+  promptQuestions(answersCallback);
 });
-
-cleanDir(cli.input[0]);
--- index.js
+++ index.js
@@ -1,85 +1,186 @@
 'use strict';
 
 const readdir = require('fs').readdir;
-const statsSync = require('fs').statSync;
+const statSync = require('fs').statSync;
 const pathJoin = require('path').join;
-
-let pkgname = require('./package.json').name;
-
+const pathResolve = require('path').resolve;
 const _ = require('underscore');
+const colors = require('colors');
 const async = require('async');
 const rimraf = require('rimraf');
+const pkgname = require('./package.json').name;
 const debug = require('debug')(pkgname);
 
-const defaultOptions = {
-  dir: '.',
-  ignore: [
-    '.git',
-    '.github',
-    '.gitlab',
-    pkgname,
-    'package.json',
-    'server.js',
-    'index.js',
-    '*.md',
-    'LICENSE'
-  ],
-  include: [
-    '.idea',
-    '.DS_Store',
-    '.tmp',
-    'npm-debug.log',
-    'package-lock.json',
-    'yarn-error.log',
-    'logs',
-    'node_modules'
-  ]
-};
+function _validateParentModule(options) {
+  const srcPath = pathResolve(options.dir, '..', '..');
+  let src = srcPath.split('/').pop() || null;
 
-function _getConfigOptions(options) {
-  debug(`\nUser Options: ${JSON.stringify(options, null, 4)}`);
-  let opts = require('rc')(pkgname, defaultOptions);
+  debug('Module Source: %s', src);
+  debug('Module Source Path: %s', srcPath);
 
-  if (_.isObject(options))
-    opts = _.extend(opts, options);
-  else if (_.isString(options)) {
-    opts = _.extend(opts, {
-      dir: options
+  if (src === 'node_modules')
+    src = pathResolve(options.dir, '..', '..', '..');
+  return null;
+}
+
+class Cleanser {
+
+  constructor (dir, callback) {
+    this.setup = false;
+    this.name = pkgname;
+    this.config = {
+      dir: '.',
+      ignore: [
+        '.git',
+        '.github',
+        '.gitlab',
+        '.cleanserrc',
+        '.travis.yml',
+        pkgname,
+        'package.json',
+        'server.js',
+        'index.js',
+        '*.md',
+        'LICENSE'
+      ],
+      include: [
+        '.idea',
+        '.DS_Store',
+        '.tmp',
+        'npm-debug.log',
+        'yarn-error.log'
+      ]
+    };
+    this.sourcePaths = {
+      directories: [],
+      files: [],
+      removing: [],
+      ignoring: []
+    };
+    this.suppliedOptions = dir;
+
+    this.getDefaultConfigs = _.bind(this.getDefaultConfigs, this);
+    this.getSourcePaths = _.bind(this.getSourcePaths, this);
+    this.getOptions = _.bind(this.getOptions, this);
+    this.setConfigurations = _.bind(this.setConfigurations, this);
+    this.setSourcePaths = _.bind(this.setSourcePaths, this);
+    this.setSourcePath = _.bind(this.setSourcePath, this);
+    this.removeSourcePaths = _.bind(this.removeSourcePaths, this);
+    this.start = _.bind(this.start, this);
+
+    this.start(callback);
+  }
+
+  setConfigurations (callback) {
+    debug(`\nSupplied Options: %O`, this.suppliedOptions);
+    this.options = require('rc')(this.name, this.config);
+
+    if (_.isObject(this.suppliedOptions))
+      this.options = _.extend(this.options, this.suppliedOptions);
+    else if (_.isString(this.suppliedOptions)) {
+      this.options = _.extend(this.options, {
+        dir: this.suppliedOptions
+      });
+    }
+
+    callback(null, this.options);
+  }
+
+  getDefaultConfigs () {
+    return this.config;
+  }
+
+  getSourcePaths () {
+    return this.sourcePaths;
+  }
+
+  getOptions () {
+    return this.options;
+  }
+
+  moveToIgnore (sources, callback) {
+    let self = this;
+
+    async.each(sources, (src, cb) => {
+      let index = _.findIndex(self.sourcePaths.removing, src);
+      self.sourcePaths.removing.splice(index, 1);
+      self.sourcePaths.ignoring.push(src);
+      cb();
+    }, err => {
+      if (err) {
+        debug('A file failed to process');
+        return callback(err);
+      }
+      debug('Source files are now ignored');
+      callback(null, self.sourcePaths);
     });
   }
 
-  debug(`\nUsing Options:\n%O`, opts);
-  return opts;
-}
+  setSourcePaths (callback) {
+    const self = this;
+    const pm = _validateParentModule(this.options);
+    if (pm) this.options.dir = pm;
 
-function Cleanser (dir, callback) {
-  const opts = _getConfigOptions(dir);
+    readdir(this.options.dir, (err, files) => {
+      if (err) return callback(err);
+      if (files.length === 0) return callback(null, 'Directory is Already Empty');
+      async.each(files, self.setSourcePath, callback);
+    });
+  }
 
-  async.waterfall([
-    cb => readdir(opts.dir, cb),
-    (files, cb) => {
-      if (files.length === 0) return cb();
-      let filesDirs = [];
+  setSourcePath (file, callback) {
+    let src = pathJoin(this.options.dir, file);
+    const stats = statSync(src);
 
-      for (let x = 0; x < files.length; x++) {
-        let src = pathJoin(opts.dir, files[x]);
-        const stats = statsSync(src);
-
-        if (stats.isDirectory() && src === pkgname)
-          continue;
-        if (stats.isDirectory() && _.findIndex(opts.ignore, src))
-          continue;
-        if (_.findIndex(opts.include, src))
-          filesDirs.push(src);
-      }
-
-      async.each(filesDirs, (src, cb) => {
-        debug('Deleting Source Path: %s', src);
-        rimraf(src, cb);
-      }, cb);
+    if (stats.isDirectory()) {
+      this.sourcePaths.directories.push(src);
+    } else {
+      this.sourcePaths.files.push(src);
     }
-  ], callback);
 
+    let found = _.find(this.options.ignore, (source) => {
+      return source === src;
+    });
+
+    if (file === this.name || found) {
+      debug('Ignoring Source Path: %s', src);
+      this.sourcePaths.ignoring.push(src);
+      return callback(null);
+    }
+
+    found = _.find(this.options.include, (source) => {
+      return (source === src || src.startsWith(source));
+    });
+
+    if (found) this.sourcePaths.removing.push(src);
+    callback(null);
+  }
+
+  removeSourcePaths (callback) {
+    debug('Source Paths:\n%O\n', this.sourcePaths);
+    async.each(this.sourcePaths.removing, (src, cb) => {
+      debug('Deleting Source Path: %s', src);
+      rimraf(src, cb);
+    }, callback);
+  }
+
+  start (callback) {
+    const self = this;
+
+    async.series([
+      this.setConfigurations,
+      this.setSourcePaths
+    ], err => {
+      if (err) {
+        debug('%s: %O', colors.red('Cleanser Error'), err);
+        return callback(err);
+      }
+      debug('Cleanser Setup Complete');
+      self.setup = true;
+      debug(`\nUsing Options:\n%O`, self.options);
+      callback();
+    });
+  }
 }
 
 module.exports = Cleanser;
